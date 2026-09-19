b34f4e357affdc59fcbd0d01ed0801a9c7d63cb5
krista1234y <boardpets123@gmail.com>
2026-09-19 13:24:47 -0700

    Import fashionProducts from fashion-products.js

--- fashion-3d.js
+++ fashion-3d.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { fashionProducts } from "./fashion-products.js";
 
 
 /* =========================================================
