97c460df47160c9cf2de4ce94046c9c6c59c733c
krista1234y <boardpets123@gmail.com>
2026-09-22 06:11:20 -0700

    Clear image path for Bohemian Halter Top
    Updated the image path for the Teal Bohemian Halter Top.

--- fashion-2d.js
+++ fashion-2d.js
@@ -8,7 +8,7 @@ const fashionLooks = {
         label: "Bohemian",
         top: {
             name: "Teal Bohemian Halter Top",
-            image: "fashion-assets/bohemian-top.png"
+            image: ""
         },
         bottom: {
     name: "Brown Lace-Up Shorts",
