439a5925abcd64c0e7e3e8233cecb7c6420f403e
krista1234y <boardpets123@gmail.com>
2026-09-22 05:55:01 -0700

    Update image path for Brown Lace-Up Shorts

--- fashion-2d.js
+++ fashion-2d.js
@@ -12,7 +12,7 @@ const fashionLooks = {
         },
         bottom: {
     name: "Brown Lace-Up Shorts",
-    image: "https://raw.githubusercontent.com/Aiinvents/Aiinventionshunting/refs/heads/Areas-ofinterest/fashion-assets/bomenian_shorts.png"
+    image: "fashion-assets/bomenian_shorts.png"
 },
         shoes: null,
         accessory: null
