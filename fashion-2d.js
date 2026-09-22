a96755f55357476c0bdc9f6bb6f9ee360f47eca0
krista1234y <boardpets123@gmail.com>
2026-09-21 23:02:06 -0700

    Fix image path for Brown Lace-Up Shorts

--- fashion-2d.js
+++ fashion-2d.js
@@ -12,7 +12,7 @@ const fashionLooks = {
         },
         bottom: {
             name: "Brown Lace-Up Shorts",
-            image: "fashion-assets/bohemian-shorts.png"
+            image: "fashion-assets/bohemian_shorts.png"
         },
         shoes: null,
         accessory: null
